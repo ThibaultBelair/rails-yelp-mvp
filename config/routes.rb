70c7dd69bed8335226bb8e2007f01659a9e97e90
thibault belair <belair.thibault@gmail.com>
2019-08-14 14:40:57 +0200

    ok controller et views restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-resources :restaurants, only: [:index, :create, :new, :show]  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :create, :new, :show] do
+    resources :reviews, only: [:show, :new, :create]
+  end  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
 
 # resources :reviews, only: [ :new, :create ] # Those routes needs restaurant_id
